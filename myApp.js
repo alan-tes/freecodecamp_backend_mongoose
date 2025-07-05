7cf734c77f161495081ec284f677ac445854f054
Alan <alan.aquino@HzTelecom.local>
2025-07-05 11:37:20 -0300

    first commit

--- myApp.js
+++ myApp.js
@@ -4,7 +4,22 @@ require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  number: {
+    type: Number,
+    required: true
+  },
+  favoriteFoods: {
+    type: [String],
+    required: true
+  }
+})
+
+let Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
