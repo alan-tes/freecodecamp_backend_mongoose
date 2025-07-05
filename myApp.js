1c9fb209e1d30e310007e66ccf8eb5ccf740a007
Alan <alan.aquino@HzTelecom.local>
2025-07-05 11:41:54 -0300

    Changes

--- myApp.js
+++ myApp.js
@@ -5,19 +5,10 @@ require('dotenv').config();
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  number: {
-    type: Number,
-    required: true
-  },
-  favoriteFoods: {
-    type: [String],
-    required: true
-  }
-})
+ name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+  })
 
 let Person = mongoose.model('Person', personSchema);
 
