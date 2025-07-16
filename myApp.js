1a9fa456419ff2e74a6532d18b60614df2a4a229
Alan <alan.aquino@HzTelecom.local>
2025-07-16 14:42:06 -0300

    Changes

--- myApp.js
+++ myApp.js
@@ -95,9 +95,9 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
- Person.remove({name: nameToRemove},  (err, removedItems) => {
+ Person.remove({name: nameToRemove},  (err, response) => {
       if(err) return console.log(err);
-      done(null, removedItems)
+      done(null, response)
     })
 };
 
